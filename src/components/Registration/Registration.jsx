d1bac910620498419de31648500dfc043f1971e1
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-11-23 15:34:39 -0600

    Started to change the registation form and changed the styling more.

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,46 +1,86 @@
-import React from 'react'
 import './Registration.css'
-import { useState } from 'react'
-
-const Registration = () => {
-
-    const [formValues, setFormValues] = useState({
-        firstname: '',
-        lastname: '',
-        username: '',
-        email: '',
-        password: '',
-        phonenumber: ''
-    })
-
-    const handleChange = (event) => {
-        setFormValues({...formValues, [event.target.name]: event.target.value})
-    }
-
-    const handleSubmit = (event) =>{
-        event.prevent.default()
-        console.log(formValues)
-    }
-
-    return ( 
-
-    <div>
-        <form onSubmit={handleSubmit}>
-            <label>First Name</label>
-            <input type= 'text'  onChange={handleChange} ></input>
-            <label>Last Name</label>
-            <input type= 'text'  onChange={handleChange} ></input>
-            <label>User Name</label>
-            <input type= 'text'  onChange={handleChange} ></input>
-            <label>Email</label>
-            <input type= 'text'  onChange={handleChange} ></input>
-            <label>Password</label>
-            <input type= 'text'  onChange={handleChange} ></input>
-            <button type ='submit'>Submit</button>
-        </form>
+import React, {useState} from 'react';
+function Registration(props) {
+    const [state , setState] = useState({
+        firstname: "",
+        lastname: "",
+        username: "",
+        password: "",
+        email: "",
+        phonenumber: ""
         
-    </div> 
-    );
+    })
+    const handleChange = (e) => {
+        const {id , value} = e.target  
+        console.log(e) 
+        setState(prevState => ({
+            prevState,
+            [id] : value
+        }))
+    }
+
+    const handleSubmitClick = (e) => {
+        e.preventDefault();
+        if(state.password === state.confirmPassword) {
+            sendDetailsToServer()    
+        } else {
+            props.showError('Passwords do not match');
+        }
+    }
+
+    return(
+      <div>
+      
+      <input type="email" 
+                       className="form-control" 
+                       id="email" 
+                       aria-describedby="emailHelp" 
+                       placeholder="Enter email" 
+                       value={state.email}
+                       onChange={handleChange}
+                />
+      
+      <input type="password" 
+                        className="form-control" 
+                        id="password" 
+                        placeholder="Password"
+                        value={state.password}
+                        onChange={handleChange} 
+                    />
+
+        <input type="username" 
+                       className="form-control" 
+                       id="username" 
+                       placeholder="Enter username" 
+                       value={state.username}
+                       onChange={handleChange}
+                />
+
+        <input type="firstname" 
+                       className="form-control" 
+                       id="firstname" 
+                       placeholder="Enter your first name" 
+                       value={state.firstname}
+                       onChange={handleChange}
+                />
+        <input type="lastname" 
+                       className="form-control" 
+                       id="lastname" 
+                       placeholder="Enter your last name" 
+                       value={state.lastname}
+                       onChange={handleChange}
+                />
+        <input type="phonenumber" 
+                       className="form-control" 
+                       id="phonenumber" 
+                       placeholder="Enter your phone number" 
+                       value={state.phonenumber}
+                       onChange={handleChange}
+                />
+            <button onClick={handleSubmitClick}>Submit</button>
+
+
+      </div>
+    )
 }
- 
-export default Registration;
+export default Registration
