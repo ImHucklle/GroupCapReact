c905b354290de7366c78acbb95c9606d5e1f8055
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-11-24 14:09:34 -0500

    added Login and Home component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Registration from './Registration/Registration';
+import Login from './Login/Login';
 import {BrowserRouter as Router,
     Switch,
     Route
 } from 'react-router-dom';
 
 function App() {
-    
+    const [title, updateTitle] = useState(null);
+    const [errorMessage, updateErrorMessage] = useState(null);
 
         return (
             <Router>
                 <Switch>
-                    <Route path="/register" component={Registration} />
+                    <Route path="/" exact={true}>
+                        <Registration showError={updateErrorMessage} updateTitle={updateTitle}/>
+                    </Route>
+                    <Route path="/register">
+                        <Registration showError={updateErrorMessage} updateTitle={updateTitle}/>
+                    </Route>
+                    <Route path="/login">
+                        <Login showError={updateErrorMessage} updateTitle={updateTitle}/>
+                    </Route>
                 </Switch>  
             </Router>
             
