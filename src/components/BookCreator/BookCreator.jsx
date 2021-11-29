3759672db06e73e86f933c75fd00fa441c3a05c3
Luca Leyva <imlucaleyva@gmail.com>
2021-11-29 16:05:51 -0500

    Added more logic to the Book Viewer

--- src/components/BookCreator/BookCreator.jsx
+++ src/components/BookCreator/BookCreator.jsx
@@ -7,24 +7,22 @@ import { withRouter } from "react-router-dom";
 function BookCreator() {
     async function handleSubmitClick(){
         console.log("hi")
-        let _firstName = document.getElementById("firstname").value
-        let _lastName = document.getElementById("lastname").value
-        let _username = document.getElementById("username").value
-        let _password = document.getElementById("password").value
-        let _email = document.getElementById("email").value
-        let _phoneNumber = document.getElementById("phonenumber").value
+        let _title = document.getElementById("title").value
+        let _author = document.getElementById("author").value
+        let _genre = document.getElementById("genre").value
+        let _releaseDate = document.getElementById("releaseDate").value
+        let _price = document.getElementById("price").value
     
     
         let payload =  { 
-            FirstName: _firstName,
-            LastName: _lastName,
-            UserName: _username,
-            Password: _password,
-            Email: _email,
-            PhoneNumber: _phoneNumber
+            Title: _title,
+            Author: _author,
+            Genre: _genre,
+            ReleaseDate: _releaseDate,
+            Price: _price,
          };
          
-        let res = await axios.post('https://localhost:44394/api/authentication/register', payload);
+        let res = await axios.post('https://localhost:44394/api/books/create', payload);
         console.log(res)
         return res.data
         
@@ -32,15 +30,14 @@ function BookCreator() {
     }
    
     return(
-        <div className="registration">
+        <div className="bookCreator">
             <form>
-                <input type="text" id="firstname"  placeholder="Enter your first name" />
-                <input type="text" id="lastname"  placeholder ="Enter your last name" />
-                <input type="text" id="username"  placeholder ="Enter your username" />
-                <input type="text" id="password"  placeholder ="Enter your password (must be 8 characters and must contain numbe)" />
-                <input type="text" id="email"  placeholder ="Enter your email" />
-                <input type="text" id="phonenumber" placeholder="Enter your phonenumber" />
-                <button type="submit" onClick={handleSubmitClick}>Register</button>
+                <input type="text" id="title"  placeholder="Enter the book title" />
+                <input type="text" id="author"  placeholder ="Enter the author's name" />
+                <input type="text" id="genre"  placeholder ="Enter the book genre" />
+                <input type="int" id="releaseDate"  placeholder ="Enter the release date)" />
+                <input type="int" id="price"  placeholder ="Enter the book price" />
+                <button type="submit" onClick={handleSubmitClick}>Add Book</button>
 
             </form>
         </div>
