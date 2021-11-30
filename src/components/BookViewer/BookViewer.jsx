9432b51ce46f21cc1e6e357992d3f00d3daf21c3
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-11-30 09:42:53 -0600

    Update BookViewer.jsx

--- src/components/BookViewer/BookViewer.jsx
+++ src/components/BookViewer/BookViewer.jsx
@@ -1,8 +1,44 @@
+import React, {Fragment, useEffect, useState} from 'react';
+import axios from 'axios';
+import './BookViewer.scss';
+// import {API_BASE_URL, ACCESS_TOKEN_NAME} from '../../constants/apiConstant';
+import { withRouter } from "react-router-dom";
+// import { PinDropSharp } from '@material-ui/icons';
 // import React, {useState} from 'react';
 // import axios from 'axios';
 // import './BookViewer.scss';
 
 
 
+const BookViewer = () => {
+    const [ books, setBooks] = useState({})
 
-// export default BookViewer;
+    const getbooks = async () => {
+
+        try {
+            let res = await axios.get('https://localhost:44394/api/books');
+            setBooks(res.data)
+            console.log("All books response: ",res.data)
+            // console.log(books)
+        } catch (err) {
+            console.log("An API error occured with books: ", err)
+            console.log(books)
+        }
+    }
+
+    useEffect(() => {
+        getbooks()
+    },[])
+
+    return(
+        <Fragment>
+            <h1>Browse Books</h1>
+            {books.map((book)=> (
+                <><h2>{book.title}</h2><h3>{book.author}</h3><h3>{book.genre}</h3><h3>{book.dscription}</h3><h3>{book.releaseDate}</h3><h3>{book.price}</h3><button>Add To Cart</button></>
+            ))}
+        </Fragment>
+    )
+}
+
+export default withRouter(BookViewer); 
+// export default BookViewer; 
